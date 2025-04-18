6de513f47ec30e77a3e3c83d9671eb4f19e8e5c1
Pedro Nascimento <pedronasc@engcomp.eng.br>
2025-04-18 00:48:34 +0000

    atualiza_sgaprod.sh

---
+++ atualiza_sgaprod/atualiza_sgaprod.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+##Script criado por Ricardo para atualizaç do sga em produç
+#
+
+SCRIPT="Atualiza_SGA.log"
+LOG=~/docker/nginx-sga/atualiza_sgaprod/atualiza_sgaprod-$(date +%Y.%m.%d_%H:%M).log
+
+DATAI=`date +%Y.%m.%d_%H:%M`
+echo "$DATAI - Iniciado    $SCRIPT -    OK" >> $LOG
+
+DATA=`date +%d.%m.%Y_%H:%M`
+
+
+chmod 777 ~/docker/nginx-sga/htm/sga-web/ -Rf
+
+rsync -rltpzuv --delete --stats --progress \
+  --exclude="node_modules" \
+  --exclude=".env" \
+  --exclude=".git" \
+  --exclude=".gitattributes" \
+  --exclude=".gitignore" \
+  --exclude="storage/framework/sessions/" \
+  -e ssh root@172.30.1.36:/htm/sgadev/sga-web/ ~/docker/nginx-sga/htm/sga-web/
+
+chmod 777 ~/docker/nginx-sga/htm/sga-web/ -Rf
+
+docker exec -it nginx-sga npm install
+docker exec -it nginx-sga composer install
+docker exec -it nginx-sga npm run build
+
+chmod 777 ~/docker/nginx-sga/htm/sga-web/ -Rf
+
+DATAF=`date +%Y.%m.%d_%H:%M`
+echo "$DATAF - Finalizado  $SCRIPT - OK" >> $LOG
